d25b4fb4b944a50bc8e2f8653814b60fceecfd72
RostSAS <rost@sinnersandsaints.la>
2025-07-21 00:58:15 +0300

    seo

--- frontend/server.js
+++ frontend/server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Указываем Express искать HTML в папке frontend
 app.use(express.static(path.join(__dirname)));
 
-// Пример: переход на /terms отдаёт terms/index.html
 app.get('/terms', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'terms', 'index.html'));
 });
 
-// Главная страница (если нужно)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
